f01fae11b1655dc176bf7c3d8e2d58fcfcc31af5
tyegwan <tye4703@naver.com>
2026-04-06 16:24:16 +0900

    feat:BookDAO 구현

--- src/com/tenco/library/dao/BookDAO.java
+++ src/com/tenco/library/dao/BookDAO.java
@@ -1,4 +1,104 @@
 package com.tenco.library.dao;
 
+import com.tenco.library.dto.Book;
+import com.tenco.library.util.DatabaseUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookDAO {
+
+    // 도서 추가
+    public int addBook(Book book) throws SQLException {
+        String sql = """
+                insert into books(title,author,publisher,publication_year,isbn) 
+                values(?,?,?,?,?)
+                """;
+
+        try (Connection conn = DatabaseUtil.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, book.getTitle());
+            pstmt.setString(2, book.getAuthor());
+            pstmt.setString(3, book.getPublisher());
+            pstmt.setInt(4, book.getPublicationYear());
+            pstmt.setString(5, book.getIsbn());
+
+            int rows = pstmt.executeUpdate();
+
+            return rows;
+
+
+        }
+
+
+    }
+
+    // 도서 전체 조회
+    public List<Book> getAllBooks() throws SQLException {
+        List<Book> bookList = new ArrayList<>();
+
+        String sql = """
+                    SELECT * FROM books ORDER By id ????
+                """;
+
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                bookList.add(mapToBook(rs));
+            }
+        }
+        return bookList;
+    }
+
+
+    public List<Book> searchBookByTitle(String title) throws SQLException {
+        List<Book> bookList = new ArrayList<>();
+        String sql = """
+                
+                select * from books where title like ?
+                """;
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, "%" + title + "%");
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    bookList.add(mapToBook(rs));
+                }
+            }
+        }
+        return bookList;
+    }
+
+    private Book mapToBook(ResultSet rs) throws SQLException {
+
+        return Book.builder()
+                .id(rs.getInt("id"))
+                .title(rs.getString("title"))
+                .author(rs.getString("author"))
+                .author(rs.getString("publisher"))
+                .publicationYear(rs.getInt("publication_year"))
+                .isbn(rs.getString("isbn"))
+                .available(rs.getBoolean("available"))
+                .build();
+    }
+
+    // 제목으로 도서 검색
+    public static void main(String[] args) {
+
+        try {
+            List<Book> resultlist = new BookDAO().searchBookByTitle("입문");
+
+            System.out.println(resultlist);
+            System.out.println("--------------");
+            System.out.println(new BookDAO().getAllBooks());
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
